67667341d7279bbadb8d406c3e54ba787fd8a42f
chi00068 <chi00068@umn.edu>
2025-09-11 19:10:10 -0500

    examine function existence

--- src/lib/Analyzer.cc
+++ src/lib/Analyzer.cc
@@ -76,36 +76,6 @@ void IterativeModulePass::run(ModuleList &modules) {
 	}
 	OP << "\n";
 
-	unsigned iter = 0, changed = 1;
-	while (changed) {
-		++iter;
-		changed = 0;
-		unsigned counter_modules = 0;
-		unsigned total_modules = modules.size();
-		for (i = modules.begin(), e = modules.end(); i != e; ++i) {
-			OP << "[" << ID << " / " << iter << "] ";
-			OP << "[" << ++counter_modules << " / " << total_modules << "] ";
-			OP << "[" << i->second << "]\n";
-
-			bool ret = doModulePass(i->first);
-			if (ret) {
-				++changed;
-				OP << "\t [CHANGED]\n";
-			} else
-				OP << "\n";
-		}
-		OP << "[" << ID << "] Updated in " << changed << " modules.\n";
-	}
-
-	OP << "[" << ID << "] Postprocessing ...\n";
-	again = true;
-	while (again) {
-		again = false;
-		for (i = modules.begin(), e = modules.end(); i != e; ++i) {
-			// TODO: Dump the results.
-			again |= doFinalization(i->first);
-		}
-	}
 
 	OP << "[" << ID << "] Done!\n\n";
 }
